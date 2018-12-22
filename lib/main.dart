47a152e1afe491ce2004df84f27bc0952946ba5b
hotatekaoru <kaorupochi@gmail.com>
2018-12-22 17:31:38 +0900

    :airplane: TopPageから、各チュートリアルの画面に遷移できるようにする

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,71 @@
 import 'package:flutter/material.dart';
-import './random_words_state.dart';
+import './startup_namer/app.dart';
 
 void main() => runApp(new MyApp());
 
-// StatelessWidgetは、Stateを持たないWidget
-// Stateを監視する必要があるWidgetはRandomWordsに内包させる
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Startup Name Generator',
-      theme: new ThemeData(
-        primaryColor: Colors.white,
-      ),
-      home: new RandomWords(),
+      title: 'Flutter Demo',
+      home: new TopPage(),
     );
   }
+}
+
+class TopPage extends StatefulWidget {
+  @override
+  _TopPageState createState() => new _TopPageState();
+}
+
+class _TopPageState extends State<TopPage> {
+  @override
+  Widget build(BuildContext context) {
+    final _pggeList = pageList();
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Flutter Tutorial"),
+      ),
+      body: new Center(
+        child: ListView(
+          children: List.generate(_pggeList.length, (index) {
+            final _pageItem = _pggeList[index];
+            return InkWell(
+              child: Card(
+                child: ListTile(
+                  title: Text(_pageItem.columnName),
+                  leading: Icon(Icons.insert_emoticon),
+                ),
+              ),
+              onTap: () {
+                Navigator.push(context, new MaterialPageRoute<Null>(
+                  settings: RouteSettings(name: _pageItem.routeName),
+                  builder: (BuildContext context) => _pageItem.nextScreenWidget
+                ));
+              },
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
+
+// structにしたいところだけど、Dartにはstructがない・・・
+class Page {
+  String columnName;
+  String routeName;
+  Widget nextScreenWidget;
+
+  // これがsetterの代わりになり、キーワード引数を伴ってnewできるようになる
+  @required
+  Page({this.columnName, this.routeName, this.nextScreenWidget});
+}
+
+List<Page> pageList() {
+  final list = List<Page>();
+  list.addAll([
+    Page(columnName: 'StartupNamer', routeName: 'startup_namer', nextScreenWidget: StartupNamer()),
+  ]);
+  return list;
 }
---
+++ lib/startup_namer/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import './random_words_state.dart';
+
+class StartupNamer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Startup Name Generator',
+      theme: new ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: new RandomWords(),
+    );
+  }
+}
